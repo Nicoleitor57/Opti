b1b4a2c07bdf6b2448eaeabf399ca39dfd564eac
Nicoleitor57 <115563015+Nicoleitor57@users.noreply.github.com>
2023-10-27 14:11:14 -0300

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -5,7 +5,7 @@ listaInstancias = [[5,7,6,10],[8,12,10,20],[13,18,20,30],[19,23,30,40],[24,30,40
                     [72,81,125,145],[82,92,145,165],[92,101,165,185],[102,111,185,205],[112,131,205,225]
                     ]
 a = 0
-while a < 1:
+while a < 3:
     Instancias = listaInstancias[a]
     minI = Instancias[0]
     maxI = Instancias[1]
@@ -30,14 +30,18 @@ while a < 1:
     
     #generar I puntos de bodegas en un plano 600x600 excluyendo centro de 150x150
     #generar J puntos de tiendas en el centro 150x150 del plano 600x600
-    bodegas = []
+    bodegasAzul = []
+    bodegasVerde =[]
     tiendas = []
     i = 0
     while i < I:
         x = random.randint(0,600)
         y = random.randint(0,600)
         if x < 225 or x > 375 or y < 225 or y > 375:
-            bodegas.append([x,y])
+            if x <= 100 or x >= 500 or y <= 100 or y >= 500:
+                bodegasAzul.append([x,y])
+            elif ((x < 500 and x > 100) or (y < 100 and y > 500)):
+                bodegasVerde.append([x,y])
             i += 1
     j = 0
     while j < J:
@@ -45,7 +49,8 @@ while a < 1:
         y = random.randint(225,375)
         tiendas.append([x,y])
         j += 1
-    print("bodegas: ",bodegas)
+    print("bodegasAzul: ",bodegasAzul)
+    print("bodegasVerde: ",bodegasVerde)
     print("tiendas: ",tiendas)
     
     #cada tienda satisfecha por almenos una bodega
@@ -55,6 +60,7 @@ while a < 1:
     #instalar bodega en almenos 40% de I
     
     #matriz de costos de transporte
+    bodegas = bodegasAzul + bodegasVerde
     costos = []
     i = 0
     while i < I:
@@ -66,6 +72,21 @@ while a < 1:
             j += 1
         i += 1
     print("costos de transporte: ",costos)
+    
+    #matriz de emisiones por transporte, emisiones = distancia recorrida(distancia aritmetica) * 1.5
+    emisiones = []
+    i = 0
+    while i < I:
+        emisiones.append([])
+        j = 0
+        while j < J:
+            emision = ((bodegas[i][0]-tiendas[j][0])**2+(bodegas[i][1]-tiendas[j][1])**2)**(1/2)*1.5
+            emisiones[i].append(emision)
+            j += 1
+        i += 1
+    print("emisiones por transporte: ",emisiones)
+    
+    
     
     #lista de costos de instalacion
     costosInstalacion = []
